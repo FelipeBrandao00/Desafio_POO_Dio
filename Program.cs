df6706cdbeb984989b0b7fb1f4f752f9f297fe45
Felipe Brandão <felipesbrandao13@gmail.com>
2023-12-30 18:03:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("1311111111","tijolão","111111111",64);
+Iphone iphone = new Iphone("132222222","descarregado","222222222",64);
+
+nokia.InstalarAplicativo("Zap-Zap");
+iphone.InstalarAplicativo("Discord");
